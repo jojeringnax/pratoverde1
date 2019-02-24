d29e2f66e9e226acd238d202e53063355b3c09f5
jojeringnax <hollow718@gmail.com>
2019-02-24 13:42:13 +0300

    NEWS PHOTOS AND TITLE_COLOR && NPM UPDATE

--- database/migrations/2019_02_24_103705_create_articles_table.php
+++ database/migrations/2019_02_24_103705_create_articles_table.php
@@ -1,9 +1,8 @@
-<?php
 
+<?php
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
-
 class CreateArticlesTable extends Migration
 {
     /**
@@ -19,9 +18,13 @@ class CreateArticlesTable extends Migration
             $table->string('title', 256);
             $table->text('content');
             $table->string('author', 128)->nullable();
+            $table->string('title_color', 64)->default('white');
+            $table->unsignedInteger('for_index_page_photo_id')->nullable();
+            $table->unsignedInteger('single_page_photo_id')->nullable();
+            $table->foreign('for_index_page_photo_id')->references('id')->on('files')->onDelete('SET NULL');
+            $table->foreign('single_page_photo_id')->references('id')->on('files')->onDelete('SET NULL');
         });
     }
-
     /**
      * Reverse the migrations.
      *
@@ -29,6 +32,10 @@ class CreateArticlesTable extends Migration
      */
     public function down()
     {
+        Schema::table('articles', function (Blueprint $table) {
+            $table->dropForeign('articles_single_page_photo_id_foreign');
+            $table->dropForeign('articles_for_index_page_photo_id_foreign');
+        });
         Schema::dropIfExists('articles');
     }
 }
